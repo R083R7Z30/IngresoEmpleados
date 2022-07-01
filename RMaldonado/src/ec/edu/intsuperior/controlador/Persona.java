7234cef1de69847ba2273a76cc4f6d26670d4b24
Beco <Beco@DESKTOP-MNFKMHI>
2022-06-30 20:50:24 -0500

    Inicio de programacion

---
+++ RMaldonado/src/ec/edu/intsuperior/controlador/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+/**
+ *
+ * @author Beco
+ */
+public class Persona {
+     String nombre;
+      String apellido;
+       String direccion;
+        enum TipoGenero{
+              MASCULINO, FEMENINO
+                }
+        TipoGenero genero;
+       public Persona(){
+           
+           
+       }
+}
