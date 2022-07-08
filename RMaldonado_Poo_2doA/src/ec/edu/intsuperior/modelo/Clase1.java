a0ee3b750e5c90ba965c3bf12aa4ea6525678900
Beco <Beco@DESKTOP-MNFKMHI>
2022-07-07 20:05:11 -0500

    Inicio de programacion

---
+++ RMaldonado_Poo_2doA/src/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,22 @@
+
+package ec.edu.intsuperior.modelo;
+
+public class Clase1 {
+   
+   public static void metodoClase_1(){
+       System.out.println("metodo 1 de clase");
+   }
+    public static void metodoClase_2(){
+       System.out.println("metodo 2 de clase");
+    }
+     public void metodoInstancia_1(){
+       System.out.println("metodo 1 de instancia");
+     }
+     public void metodoInstancia_2(){
+         System.out.println("metodo 2 de instancia");
+         }
+     public void metodoInstancia_3(){
+         System.out.println("metodo 3 de instancia");
+         
+     }
+}
